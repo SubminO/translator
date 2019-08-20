2f74bc6a892e173f2dbc309ddaccf6dd251f8c7a
Юрий Шикин <shikin@submin.ru>
2019-08-20 17:28:11 +0400

    чистка кода

--- wialon_reciver/server.py
+++ wialon_reciver/server.py
@@ -1,25 +1,10 @@
 # -*- coding: utf-8 -*-
-# import redis
-# import binascii
-# import pytz
+
 import socket
-# import os
 import json
-# import requests
 import threading
 from websocket_server import WebsocketServer
-# import websocket
-# from simple_websocket_server import WebSocketServer, WebSocket
-import logging
-
-# from lxml import etree
-# from datetime import datetime, timedelta
-
 from wialon_parser import parse, parsePacket
-
-
-# redis_tracks = redis.StrictRedis(host='redis', port=6379, db=0)
-# redis_data_count = redis.StrictRedis(host='redis', port=6379, db=1)
 
 
 def on_new_client(sck, addr, wsserver):
@@ -47,8 +32,6 @@ def on_new_client(sck, addr, wsserver):
 
             packet_size = parse('<I', queue)
 
-            # packet was received successfully
-
         # on_message(messages, client)
         tracks_json = json.dumps(messages)
 
@@ -64,26 +47,22 @@ def on_new_client(sck, addr, wsserver):
 CONNECTION = ('0.0.0.0', 20163)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# prevent 'ERROR: Address already in use'
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
 server.bind(CONNECTION)
 server.listen(1)
 
-# wsserver = WebsocketServer(8091, host='0.0.0.0')
-wsserver = WebsocketServer(8091, host='0.0.0.0', loglevel=logging.INFO)
-# wsserver.set_fn_new_client(new_client)
-wsserver.run_forever()
+ws_server = WebsocketServer(8091, host='0.0.0.0')
+ws_thread = threading.Thread(target=ws_server.run_forever)
+ws_thread.start()
 
 print("Listen {0} on {1}".format(*CONNECTION))
 
-sck = None
+sock = None
 
 while True:
-    sck, addr = server.accept()
+    sock, addr = server.accept()
     print("Connected {0}:{1}".format(*addr))
-    my_thread = threading.Thread(target=on_new_client, args=(sck, addr, wsserver,))
-    my_thread.start()
+    main_thread = threading.Thread(target=on_new_client, args=(sck, addr, ws_server,))
+    main_thread.start()
 
-sck.close()
+sock.close()
--- wialon_reciver/wialon_parser.py
+++ wialon_reciver/wialon_parser.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import binascii
 import struct
 from datetime import datetime
+
 
 def parse(fmt, binary, offset=0):
     """
@@ -20,20 +20,17 @@ def parsePacket(packet):
     """
     Parse Wialon Retranslator v1.0 packet w/o first 4 bytes (packet size)
     """
-
     # parsed message
     msg = {
         'uid': 0,
         'time': '',
-        # 'flags': 0,
         'params': {},
-        # 'blocks': []
     }
 
     # parse packet info
     controller_id_size = packet.find('\x00')
     (msg['uid'], time, flags) = parse('> %ds x i i' % (controller_id_size), packet)
-    msg['time'] = datetime.fromtimestamp(int(time)).strftime("%d-%m-%Y %H:%M:%S")
+    msg['time'] = datetime.fromtimestamp(int(time)).strftime("%Y-%m-%d %H:%M:%S")
 
     # get data block
     data_blocks = packet[controller_id_size + 1 + 4 + 4:]
@@ -49,7 +46,6 @@ def parsePacket(packet):
                  'name': name, 'data_block': data_blocks[offset + name_size + 1:block_length * 1 + 6]}
 
         # get block data
-
         v = ''
         # todo сделать определение что разбирать на основе flags http://extapi.wialon.com/hw/cfg/WialonRetranslator%201.0.pdf
         if data_type == 1:
@@ -59,7 +55,7 @@ def parsePacket(packet):
         if data_type == 2:
             # binary
             if name == 'posinfo':
-                v = {'lon': 0, 'lat': 0, 'h': 0, 's': 0, 'c': 0, 'sc': 0}
+                v = {}
                 (v['longitude'], v['latitude'], v['altitude']) = parse('d d d', block['data_block'])
                 (v['speed'], v['course'], v['satellites']) = parse('> h h b', block['data_block'], 24)
         elif data_type == 3:
@@ -75,21 +71,7 @@ def parsePacket(packet):
         # add param to message
         msg['params'][name] = v
 
-        # data blocks parse information
-        # msg['blocks'].append(block)
-
         # delete parsed info
         data_blocks = data_blocks[block_length + 6:]
 
     return msg
-
-
-if __name__ == '__main__':
-    # test data
-    data = [
-        '333533393736303133343435343835004B0BFB70000000030BBB000000270102706F73696E666F00A027AFDF5D9848403AC7253383DD4B400000000000805A40003601460B0BBB0000001200047077725F657874002B8716D9CE973B400BBB00000011010361766C5F696E707574730000000001',
-        '73686d6900552d3f49000000070bbb000000270102706f73696e666f001f090e42538d3b40af8c20a82dfc4a400000000000000000006c0109ff0bbb0000000f000461646331000000000000ca21400bbb00000011010361766c5f696e7075747300000000240bbb00000012010361766c5f6f7574707574730000000037'
-    ]
-    for i in range(len(data)):
-        print('\nParse packet: %s\n' % data[i])
-        print(parsePacket(binascii.unhexlify(data[i])))
